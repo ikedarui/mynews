0ca288126507af837b6de266b2f96870177c6c36
池田琉生 <ruisandes@i.softbank.jp>
2021-08-03 05:00:21 +0000

    課題１２

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' =>'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     
 //課題４
-Route::get('profile/create', 'Admin\ProfileController@add');
-Route::get('profile/edit', 'Admin\ProfileController@edit');
+Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
