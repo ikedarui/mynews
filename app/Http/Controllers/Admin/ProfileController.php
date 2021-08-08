a9cc66c297323281a0cdde55e19a33556d63e84c
池田琉生 <ruisandes@i.softbank.jp>
2021-08-08 05:05:37 +0000

    課題14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,14 +5,32 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
     {
         return view('admin.profile.create');
     }
-    public function create()
+    
+    
+    public function create(Request $request)
     {
+        // 以下を追記
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // データベースに保存する
+      
+      $profile->fill($form);
+      $profile->save();
+        
         return redirect('admin/profile/create');
     }
     public function edit()
