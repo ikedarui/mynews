34e4f56cc51602bcaff6866a7affa2e08f5c9117
池田琉生 <ruisandes@i.softbank.jp>
2021-08-26 12:14:34 +0000

    Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //以下を追記
+        if (\App::environment('production')) {
+            \URL::ferceScheme('https');
+        }
     }
 }
